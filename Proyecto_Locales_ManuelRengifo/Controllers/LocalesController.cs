5a842648ecd8613e39194cfb0954d4f43dcfa6e6
InformaticNuke <InformaticNuke@gmail.com>
2023-05-26 03:38:01 -0400

    Testing
    Just some random testing shit

---
+++ Proyecto_Locales_ManuelRengifo/Controllers/LocalesController.cs
@@ -0,0 +1,105 @@
+﻿using Proyecto_Locales_ManuelRengifo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Locales_ManuelRengifo.Controllers
+{
+    public class LocalesController : Controller
+    {
+        BasePruebaBDEntities conexion = new BasePruebaBDEntities();
+        public ActionResult Index()
+        {
+
+            var lista = conexion.Locales.ToList();
+            return View(lista);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Locales local = conexion.Locales.Find(id);
+
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "IdLocal,NombreLocal,Direccion,Telefono")]Locales local)
+        {
+            if (!ModelState.IsValid)
+            {
+                conexion.Locales.Add(local);
+                conexion.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(local);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Locales local = conexion.Locales.Find(id);
+
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        public ActionResult Edit([Bind(Include = "IdLocal,NombreLocal,Direccion,Telefono")] Locales local)
+        {
+            if (ModelState.IsValid)
+            {
+                conexion.Entry(local).State = EntityState.Modified;
+                conexion.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(local);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Locales local = conexion.Locales.Find(id);
+
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Locales local = conexion.Locales.Find(id);
+            conexion.Locales.Remove(local);
+            conexion.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Proyecto_Locales_ManuelRengifo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Locales_ManuelRengifo.Startup))]
+namespace Proyecto_Locales_ManuelRengifo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
